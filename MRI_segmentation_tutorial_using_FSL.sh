1d03b11370575edfd928f6da1d7c15ada7d0ad86
Piyali Karmakar <karmakarpiyali275@gmail.com>
2025-06-05 17:20:01 +0530

    Create MRI_segmentation_tutorial_using_FSL.sh

---
+++ MRI_segmentation_tutorial_using_FSL.sh
@@ -0,0 +1,44 @@
+"
+FAST:
+-----
+FSL's 'fast' CLI tool can segment MRIs into Gray Matter(GM), White Matter(WM), Cerebro-Spinal Fluid(CSF). It 
+Fast expects Skullstripped images
+"
+# For brain segmentation using FSL's 'bet':
+bet </path/to/t1.nii.gz> </path/to/t1_skullstripped.nii.gz> -f 0.02 -g 0 -m -B
+"
+'-B' is for bias field correction
+-f 0.02 is the threshold by which the skull is stripped
+"
+
+# For segmenting using 'fast':
+fast -B -o <full/path/to/output/directory>/<Prefix> <t1_skullstripped.nii.gz> 
+"
+'Prefix' is the prefix for all segmented images
+'-B' indicates the bias-corrected image 
+"
+
+
+"
+FIRST:
+------
+FIRST is FSL’s tool for automatic segmentation of a number of subcortical structures. The supported structures are:
+
+    Putamen (Puta)
+    Caudate nucleus (Caud)
+    Nucleus accumbens (Accu)
+    Globus pallidus (Pall)
+    Hippocampus (Hipp)
+    Amygdala (Amyg)
+    Thalamus (Thal)
+    Brainstem (BrStem) 
+"
+run_first_all -i </full/path/to/output/directory> -o <full/path/to/output/directory>/<Prefix>
+"
+
+
+1. https://web.mit.edu/fsl_v5.0.10/fsl/doc/wiki/FAST.html
+2. https://open.win.ox.ac.uk/pages/fslcourse/lectures/additional/2024/2-seg_part1.pdf
+3. https://web.mit.edu/fsl_v5.0.10/fsl/doc/wiki/FIRST(2f)StepByStep.html
+4. https://web.mit.edu/fsl_v5.0.10/fsl/doc/wiki/FIRST(2f)UserGuide.html
+"
